74bb6474a329091d3168eac1a7eb9ad429569238
Camille <gryspeerdt.camille@gmail.com>
2023-04-12 17:38:13 +0200

    maj de model user alias en username pour faire fonctionner les route

--- models/users.js
+++ models/users.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   firstname: String,
-  alias: String,
+  username: String,
   email: String,
   image: String,
   password: String,
   age: Date,
   gender: String,
   dateCreation: Date,
-  followedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
+  followedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
   token: String,
 });
 
-const User = mongoose.model('users', userSchema);
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
