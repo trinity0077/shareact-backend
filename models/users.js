45da0b4a2d9fae3110e81e5a717c4fc26502a6e2
Camille Gryspeerdt <124784939+trinity0077@users.noreply.github.com>
2023-11-06 14:58:10 +0100

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,15 +1,5 @@
 const mongoose = require("mongoose");
 
-const smokecigarettesSchema = mongoose.Schema({
-  smokedCig: Number,
-  dateSmoked: Date,
-});
-
-const notSmokedcigarettesSchema = mongoose.Schema({
-  notSmokedCig: Number,
-  datenotSmoked: Date,
-});
-
 const userSchema = mongoose.Schema({
   firstname: String,
   username: String,
@@ -21,8 +11,6 @@ const userSchema = mongoose.Schema({
   dateCreation: Date,
   followedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
   token: String,
-  smokeCigarettes: [smokecigarettesSchema], // Utilisation du schéma des cigarettes fumées comme sous-document
-  NotSmokedcigarettes: [notSmokedcigarettesSchema]
 });
 
 const User = mongoose.model("users", userSchema);
